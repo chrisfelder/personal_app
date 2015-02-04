cec92842d1dd15674ebcbd60efa408a911dd8527
chrisfelder <cdf003@shsu.edu>
2015-02-04 16:02:12 +0000

    Update QuickSort algorithm completed

--- app/helpers/experiments_helper.rb
+++ app/helpers/experiments_helper.rb
@@ -101,7 +101,8 @@ module ExperimentsHelper
     else
       pivot = choose_pivot(array, length)
       #array = partition(array, pivot, length - 1)
-    
+      
+      #make sure to switch the element at the pivot index with the first element
       
       i = pivot + 1
       j = pivot + 1
@@ -129,6 +130,33 @@ module ExperimentsHelper
     return 0
   end
   
+  def choose_pivot_last(array, length)
+    return (array.length - 1)
+  end
+  
+  def choose_pivot_median(array, length)
+    if length == 2 || array.length == 2
+      return 0
+    end
+    
+    half = ((array.length + 1) / 2) - 1
+    first = array.first
+    middle = array[half]
+    #array.length % 2 == 0 ? middle = array[half - 1] : middle = array[half] 
+    last = array.last
+    
+    if first >= last && first <= middle || first <= last && first >= middle
+      return 0
+    elsif last > first && last < middle || last < first && last > middle
+      return length - 1
+    else
+      return half
+    end
+    
+    
+    
+  end
+  
   # 3 8 2 5 1 4 7 6 or # 2 1 4 3
   def partition(array, left, right)
 
@@ -145,4 +173,72 @@ module ExperimentsHelper
     array[left], array[i - 1] = array[i - 1], array[left]
     return array
   end
+  
+  
+  
+  def quick_sort_count(array)
+    #puts "This is the array entering quicksortx: " + array.to_s
+    temp = quick_sortx(array, array.length)
+    puts "And this is the sum: " + temp[1].to_s
+    puts "This is the final array: " + temp[0].to_s
+    return temp[1]
+  end
+  
+  def quick_sortx(array, length)
+     
+    if length == 1 || length == 0
+      return [array, 0]
+    else
+      pivot = choose_pivot_last(array, length)
+      array[pivot], array[0] = array[0], array[pivot]
+      pivot = 0
+      
+      sub_array = []
+      i = pivot + 1
+      j = pivot + 1
+      for x in j..length - 1
+        if array[j] < array[pivot]
+          array[j], array[i] = array[i], array[j]
+          i += 1
+        end
+        j += 1
+      end
+      
+      array[pivot], array[i - 1] = array[i - 1], array[pivot]
+      #puts "This is the array after partitioning: " + array.to_s
+
+
+      i > 1 ? sub_left = array[0..(i - 2)]: sub_left = []
+      #puts "This is the sub_left going into the left recursive call: " + sub_left.to_s
+      sub_right = array[i..(length - 1)]
+      #puts "This is the sub_right going into the right recursive call: " + sub_right.to_s
+      
+      #recursively sort array < p
+      left = quick_sortx(sub_left, i - 1)
+      #puts "This is the left array after quicksortx: " + left[0].to_s
+      #i > 1 ? left[1] += (i - 1) : left[1] += 1
+      
+      #recursively sort array > p
+      right = quick_sortx(sub_right, (length - i))
+      #right[1] += (right[0].length - 1)
+      
+      
+      left[0].each do |z|
+        sub_array << z
+      end
+      
+      #puts "After appending left sub_array: " + sub_array.to_s
+      sub_array << array[i - 1]
+      
+      right[0].each do |y|
+        sub_array << y
+      end
+      
+      #puts "After appending right sub_array: " + sub_array.to_s
+      sum = (sub_array.length - 1) + left[1] + right[1]
+      return [sub_array, sum]
+    end
+  end
+  
+  
 end
--- test/models/experiment_test.rb
+++ test/models/experiment_test.rb
@@ -1,26 +1,37 @@
 require 'test_helper'
 
 class ExperimentTest < ActiveSupport::TestCase
-  include ExperimentsHelper
-  array = []
-  file = File.open(Rails.root.to_s + "/lib/assets/algorithms/quicksort.txt", "r")
-  file.each_line do |line|
-    array << line.to_i
-  end
+  #include ExperimentsHelper
+  #array = []
+  #file = File.open(Rails.root.to_s + "/lib/assets/algorithms/quicksort.txt", "r")
+  #file.each_line do |line|
+  #  array << line.to_i
+  #end
+  #file.close
   
-  test "run quick_sort" do
-    assert_equal [1, 2, 3, 4], quick_sort([2,1, 4, 3], 4), "Should return a sorted array"
-  end
+  #test "run quick_sort" do
+  #  assert_equal [1, 2, 3, 4], quick_sort([2,1, 4, 3], 4), "Should return a sorted array"
+  #end
   
-  test "run quick_sort_full" do
-    assert_equal [1, 2, 3, 4, 5, 6, 7, 8], quick_sort([3, 8, 2, 5, 1, 4, 7, 6], 8)
-  end
+  #test "run quick_sort_full" do
+  #  assert_equal [1, 2, 3, 4, 5, 6, 7, 8], quick_sort([3, 8, 2, 5, 1, 4, 7, 6], 8)
+  #end
 
   #test "run partition" do
   #  assert_equal [1, 2, 3, 5, 8, 4, 7, 6], partition([3, 8, 2, 5, 1, 4, 7, 6], 0, 7)
   #end
   
-  test "run quicksort on small sample file" do
-    assert_equal [609, 2148, 3153, 6324, 7628, 7742, 9058], quick_sort(array, 7)
-  end
+  #test "run quicksort on small sample file" do
+  #  assert_equal [609, 2148, 3153, 6324, 7628, 7742, 9058], quick_sort(array, 7)
+  #end
+  
+  #filex = File.open(Rails.root.to_s + "/lib/assets/algorithms/testcase1.txt", "r")
+  #filex.each_line do |line|
+  #  array << line.to_i
+  #end
+  #filex.close
+  #test "run quicksort count on small sample file" do
+  #  assert_equal 21, quick_sort_count(array)
+  #end
+  
 end
